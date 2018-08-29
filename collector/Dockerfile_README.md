3464dd8ed1de4fac76830116a50fd181e98fafbf
Patrick Brooks <patrick.n.brooks@gmail.com>
2018-08-28 22:07:15 -0400

    counting number of branches in pre-cloned, hardcoded repos

--- collector/Dockerfile_README.md
+++ collector/Dockerfile_README.md
@@ -12,7 +12,7 @@ docker images
 
 3. Build the Docker image
 ```
-docker build -t git-branch-metric-collector .
+docker build -t git-branch-collector .
 ```
 
 4. See the new docker image on your system
@@ -22,7 +22,7 @@ docker images
 
 5. Start a container from the image.
 ```
-docker run -it --rm git-branch-metric-collector
+docker run -it --mount type=bind,source='/Users/pb/Git',target='/root/Git' git-branch-collector
 ```
 
 6. For starters, run the script by hand
@@ -30,3 +30,12 @@ docker run -it --rm git-branch-metric-collector
 python git-branch-metrics-collector.py
 exit
 ```
+
+
+## During Development
+
+During development, build the image as described above. Then, when running the image, mount the host's repo directory under the container's /root/dev directory using this command.
+```
+docker run -it --mount type=bind,source='/Users/pb/Git/git-branch-metrics',target='/root/dev' --mount type=bind,source='/Users/pb/Git',target='/root/Git' git-branch-collector
+```
+After you edit the file on the host, execute the updated script in the container's /root/dev directory.
--- collector/git-branch-metrics-collector.py
+++ collector/git-branch-metrics-collector.py
@@ -1,8 +1,83 @@
 #!/usr/bin/env python3
+"""
+git-branch-metrics-collector.py
+"""
+
+import argparse
+import logging
+import os
 import sh
+import sys
 
-#git = sh.git.bake(_cwd='/home/pb/Git/temp')
+# Prepare the logger
+log = logging.getLogger(__name__)
+stdout_handler = logging.StreamHandler(sys.stdout)
+log.addHandler(stdout_handler)
 
-#print(git.status())
 
-print("Hello World")
+def parse_cmd_line():
+    parser = argparse.ArgumentParser(description="Count metrics in git repos")
+    parser.add_argument('--path',
+                        help="absolute path inside container of repos' parent directory",
+                        default="/root/Git")
+    parser.add_argument('--log',
+                        help='set logging level (default is warn)',
+                        choices=['info', 'debug'],
+                        default='warn')
+    args = parser.parse_args()
+
+    if args.log == 'debug':
+        log.setLevel(logging.DEBUG)
+    elif args.log == 'info':
+        log.setLevel(logging.INFO)
+    else:
+        # default log level is WARN
+        log.setLevel(logging.WARN)
+
+    return {'repo_path': args.path}
+
+
+def count_branches(git):
+
+    # branch_list includes an extra line of output from the 'git branch'
+    # command. strip() does not get rid of it.
+    branch_list = git.branch('-a').split('\n')
+    log.debug(branch_list)
+
+    print('Count of branches in repo', repo, '=', len(branch_list)-1)
+    for branch in branch_list:
+        log.info(branch.strip())
+
+
+def collect_repo_stats(repo_path, repo):
+
+    repo_path = os.path.join(repo_path, repo)
+
+    if not os.path.isdir(repo_path):
+        log.error("Repo path is not a directory = %s" % repo_path)
+
+    else:
+        git = sh.git.bake(_cwd=repo_path)
+
+        # receive new branches and delete old branches
+        git.fetch('--all', '--prune')
+
+        count_branches(git)
+
+
+if __name__ == '__main__':
+
+    print("\nExecuting git-branch-metrics-collector.py ...\n")
+
+    args = parse_cmd_line()
+    log.debug(args)
+
+    repos = [
+        'git-branch-metrics',
+        'temp'
+    ]
+
+    for repo in repos:
+        collect_repo_stats(args['repo_path'], repo)
+
+    print("\nDone\n")
